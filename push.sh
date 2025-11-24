a933a18ceda29a76d4ae1ad1f6e949e757eace7a
Zhang Junhua <1493916656@qq.com>
2025-11-24 14:33:58 +0800

    first commit

--- push.sh
+++ push.sh
@@ -1,15 +1,7 @@
+# echo "# kaggle-MABe" >> README.md
 # git init
-# git config --global --add safe.directory /kaggle-MABe
-
-# git config --global user.email "1458038842@qq.com"
-# git config --global user.name "TheKOG"
-
 git add .
 git commit -m "first commit"
-
 # git branch -M main
-
-# git remote add origin git@github.com:Jayzen-Tech/tencent-algo.git
-
-# GIT_TRACE=1 GIT_CURL_VERBOSE=1 git push
-GIT_TRACE=1 GIT_CURL_VERBOSE=1 git push origin feature/kfold-multirecall
+# git remote add origin git@github.com:Jayzen-Tech/kaggle-MABe.git
+git push -u origin main
